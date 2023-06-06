b413145a4bd1c0ed1ac6d68e65c7772f7bb3f0bb
Bhawani Singh <bhawani.singh@abjima.com>
2023-06-06 18:55:18 +0530

    Creating Login Page

---
+++ ClinicManagementSystem/ClinicManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using ClinicManagementSystem.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult SignUp(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // Call the stored procedure
+                    SignUpUser(user);
+
+                    // Redirect to a success page or perform any other desired action
+                    return RedirectToAction("SignUpSuccess");
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors that occurred during signup
+                    ModelState.AddModelError("", "An error occurred during signup.");
+
+                }
+            }
+
+            return View(user);
+        }
+
+
+        private void SignUpUser(Users user)
+        {
+            using (SqlConnection connection = new SqlConnection("DbString"))
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@UserName", user.UserName);
+                parameters.Add("@UserEmail", user.UserEmail);
+                parameters.Add("@Password", user.Password);
+                parameters.Add("@UserType", user.UserType);
+
+                connection.Execute("SignUpUser", parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ ClinicManagementSystem/ClinicManagementSystem/Models/Users.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace ClinicManagementSystem.Models
+{
+    public class Users
+    {
+        [Required]
+        public string? UserName { get; set; }
+
+        [Required]
+        public string? UserEmail { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? UserType { get; set; }
+
+
+        public void SignUp()
+        {
+            string connectionString = "DbString";
+            // Created a new SqlConnection using the connection string
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create a new SqlCommand using the stored procedure name and connection
+                using (SqlCommand command = new SqlCommand("sp_create_User", connection))
+                {
+                    // Set the command type as stored procedure
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Add the input parameters to the command
+                    command.Parameters.Add("@UserName", SqlDbType.VarChar, 50).Value = UserName;
+                    command.Parameters.Add("@UserEmail", SqlDbType.VarChar, 100).Value = UserEmail;
+                    command.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = Password;
+                    command.Parameters.Add("@UserType",SqlDbType.VarChar,50).Value= UserType;
+
+                    try
+                    {
+                        // Openening the database connection
+                        connection.Open();
+
+                        // Execute the command
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        // Handling any SQL errors as needed
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
+    
